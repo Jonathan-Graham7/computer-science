1bda0eacaa1a9b7de5161774a5ccb234f65658cc
Jonathan-Graham7 <0jlgraham@gmail.com>
2024-11-30 17:58:48 -0500

    first iteration of smaller scale, more testing to be done

--- intro_to_programming/project/project.py
+++ intro_to_programming/project/project.py
@@ -18,68 +18,7 @@ def argParser():
         metavar='[0-10]',
         default=0
     )
-    parser.add_argument(
-        '-last',
-        help='Indicates if this is the last frame of a game.',
-        action=argparse.BooleanOptionalAction,
-        default=False
-    )
     return vars(parser.parse_args())
-
-def playBall(pin_set, second_ball, score):
-    die = np.random.randint(1, 7, 3)
-    #die = np.array([2, 2, 1])
-    while True:
-        for pin in pin_set:
-            print(pin, end=' ')
-        print()
-        input_string = f"Enter an equation using {die[0]}, {die[1]}, and {die[2]} or type 'end' to complete the first half of the frame: "
-        if second_ball:
-            input_string = f"Enter an equation using {die[0]}, {die[1]}, and {die[2]} or type 'end' to complete the frame: "
-        equation_input = input(input_string)
-        if equation_input == 'end':
-            break
-        is_valid = verifyInput(equation_input, die)
-        if is_valid != 'Valid':
-            print(is_valid)
-            continue
-        try:
-            test = complexSolveInput(equation_input)
-        except ValueError:
-            print("Typo in equation")
-            continue
-        if test[1] != 'Valid':
-            print(test[1])
-            continue
-        pin_set = pinUpdate(pin_set, test[0])
-    ball_score = scoreKeeper(pin_set, score)
-    if ball_score == 10:
-        return "STRIKE!"
-    if second_ball and score + ball_score == 10:
-        return "SPARE!"
-    return ball_score
-
-def main():
-    args_given = argParser()
-    bowling_pins = initializePins(args_given['difficulty'])
-    frame_score = 0
-    first_ball = False
-    ball_1 = playBall(bowling_pins, first_ball, frame_score)
-    try:
-        first_half_frame_score = int(ball_1)
-        print(f"Scored {first_half_frame_score} points in this part of the frame")
-        frame_score += first_half_frame_score
-    except ValueError:
-        return ball_1
-    second_ball = True
-    ball_2 = playBall(bowling_pins, second_ball, frame_score)
-    try:
-        second_half_frame_score = int(ball_2)
-        print(f"Scored {second_half_frame_score} points in this part of the frame")
-        frame_score += second_half_frame_score
-    except ValueError:
-        return ball_2
-    sys.exit()
 
 def initializePins(num_to_randomize):
     pins = list(range(1, 11))
@@ -88,23 +27,23 @@ def initializePins(num_to_randomize):
         pins[num] *= -1
     return pins
 
-def pinUpdate(current_pins_up, latest_input):
-    if latest_input in current_pins_up:
-        current_pins_up[current_pins_up.index(latest_input)] = '\033[92m\u2713\033[0m'
-    return current_pins_up
-
-def scoreKeeper(current_pins_up, current_score):
-    still_up = 0
-    for pin in current_pins_up:
-        try:
-            int(pin)
-        except ValueError:
-            continue
-        else:
-            still_up += 1
-    score = 10 - still_up - current_score
-    return score
-
+def inputPins():
+    while True:
+        input_pins = input("Please list the remaining pins in the format 'integer, integer, ...': ").split(',')
+        pins = []
+        for pin in input_pins:
+            pin.strip()
+            try:
+                pin = int(pin)
+            except ValueError:
+                continue
+            else:
+                if pin > 6 or pin < 1:
+                    continue
+                pins.append(pin)
+        if len(pins) == len(input_pins):
+            return pins
+        
 def verifyInput(equation, die):
     die_list = die.tolist()
     verify_die = []
@@ -125,17 +64,26 @@ def verifyInput(equation, die):
         return "Didn't use all of the rolled die."
     return "Valid"
 
+def verifyEquationFormat(equation_character):
+    try:
+        float(equation_character)
+    except ValueError:
+        return False
+    return True
+
 def simpleSolveInput(equation):
     if len(equation) != 1:
         while True:
             if '!' in equation:
                 factorial_place = equation.index('!')
-                try:
-                    equation[factorial_place] = math.factorial(int(equation[factorial_place - 1]))
-                except ValueError:
-                    return float(equation[factorial_place - 1]), 'Tried to compute factorial on non-integer.'
+                if not verifyEquationFormat(equation[factorial_place - 1]):
+                    return equation[factorial_place - 1], 'Typo in equation'
+                factorial_number = float(equation[factorial_place - 1])
+                if round(factorial_number) < factorial_number + 0.00001 and -1 * round(factorial_number) > -1 * factorial_number + 0.00001:
+                    equation[factorial_place] = math.factorial(round(equation[factorial_place - 1]))
                 else:
-                    del equation[factorial_place - 1]
+                    return factorial_number, 'Tried to compute factorial on non-integer.'
+                del equation[factorial_place - 1]
             else:
                 break
         while True:
@@ -148,6 +96,20 @@ def simpleSolveInput(equation):
                     exponent_place = equation.index('^')
                 except ValueError:
                     exponent_place = sys.maxsize
+                if exponent_place != sys.maxsize:
+                    if not verifyEquationFormat(equation[exponent_place + 1]):
+                        return equation[exponent_place + 1], 'Typo in equation'
+                    if not verifyEquationFormat(equation[exponent_place - 1]):
+                        return equation[exponent_place - 1], 'Typo in equation'
+                if root_place != sys.maxsize:
+                    if 's' not in equation:
+                        if not verifyEquationFormat(equation[root_place + 1]):
+                            return equation[root_place + 1], 'Typo in equation'
+                        if not verifyEquationFormat(equation[root_place - 2]):
+                            return equation[root_place - 2], 'Typo in equation'
+                    else:
+                        if not verifyEquationFormat(equation[root_place + 1]):
+                            return equation[root_place + 1], 'Typo in equation'
                 if root_place < exponent_place:
                     if 's' in equation:
                         equation[root_place] = np.sqrt(float(equation[root_place + 1]))
@@ -173,6 +135,16 @@ def simpleSolveInput(equation):
                     division_place = equation.index('/')
                 except ValueError:
                     division_place = sys.maxsize
+                if multiplication_place != sys.maxsize:
+                    if not verifyEquationFormat(equation[multiplication_place + 1]):
+                        return equation[multiplication_place + 1], 'Typo in equation'
+                    if not verifyEquationFormat(equation[multiplication_place - 1]):
+                        return equation[multiplication_place - 1], 'Typo in equation'
+                if division_place != sys.maxsize:
+                    if not verifyEquationFormat(equation[division_place + 1]):
+                        return equation[division_place + 1], 'Typo in equation'
+                    if not verifyEquationFormat(equation[division_place - 1]):
+                        return equation[division_place - 1], 'Typo in equation'
                 if multiplication_place < division_place:
                     equation[multiplication_place] = float(equation[multiplication_place - 1]) * float(equation[multiplication_place + 1])
                     del equation[multiplication_place + 1]
@@ -193,6 +165,16 @@ def simpleSolveInput(equation):
                     subtraction_place = equation.index('-')
                 except ValueError:
                     subtraction_place = sys.maxsize
+                if addition_place != sys.maxsize:
+                    if not verifyEquationFormat(equation[addition_place + 1]):
+                        return equation[addition_place + 1], 'Typo in equation'
+                    if not verifyEquationFormat(equation[addition_place - 1]):
+                        return equation[addition_place - 1], 'Typo in equation'
+                if subtraction_place != sys.maxsize:
+                    if not verifyEquationFormat(equation[subtraction_place + 1]):
+                        return equation[subtraction_place + 1], 'Typo in equation'
+                    if not verifyEquationFormat(equation[subtraction_place - 1]):
+                        return equation[subtraction_place - 1], 'Typo in equation'
                 if addition_place < subtraction_place:
                     equation[addition_place] = float(equation[addition_place - 1]) + float(equation[addition_place + 1])
                     del equation[addition_place + 1]
@@ -204,12 +186,11 @@ def simpleSolveInput(equation):
             else:
                 break
     if len(equation) != 1:
-        return len(equation), f'Equation was not fully solved, still contained {equation}.'
-    if float(equation[0]) > 1.00001 * int(equation[0]): #checks for accuracy up to 5 decimal places
-        return float(equation[0]), f'Equation did not equal an integer, {equation[0]}.'
-    print(equation[0])
-    return int(equation[0]), 'Valid'
-    
+        return len(equation), f'Equation was not fully solved, still contained {equation}'
+    if round(equation[0]) < equation[0] + 0.00001 and -1 * round(equation[0]) > -1 * equation[0] + 0.00001: #checks for accuracy up to 5 decimal places
+        return float(equation[0]), f'Equation did not equal an integer'
+    return round(equation[0]), 'Valid'
+
 def complexSolveInput(equation):
     equation_list = list(equation)
     equation_list = [term for term in equation_list if term != ' ']
@@ -218,10 +199,27 @@ def complexSolveInput(equation):
         if term == 's':
             root_counter += 1
     if root_counter > 7:
-        return root_counter, f"Tried to cheat by using square root function {root_counter} times."
+        return root_counter, f"Tried to cheat by using square root function too many times"
     if '(' not in equation_list or ')' not in equation_list:
         return simpleSolveInput(equation_list)
     while True:
+        for entry in equation_list:
+            try:
+                entry.isdigit()
+                float(entry)
+            except (ValueError, AttributeError, IndexError):
+                pass
+            else:
+                if equation_list.index(entry) + 1 < len(equation_list) and equation_list[equation_list.index(entry) + 1] == '(':
+                    equation_list.insert(equation_list.index(entry) + 1, '*')
+            if entry == ')':
+                try:
+                    equation_list[equation_list.index(entry) + 1].isdigit()
+                    float(equation_list[equation_list.index(entry) + 1])
+                except (ValueError, AttributeError, IndexError):
+                    pass
+                else:
+                    equation_list.insert(equation_list.index(entry) + 1, '*')
         open_parenthesis = []
         close_parenthesis = []
         for i, char in enumerate(equation_list):
@@ -231,7 +229,7 @@ def complexSolveInput(equation):
                 close_parenthesis.append(i)
         if len(open_parenthesis) != len(close_parenthesis):
             return len(open_parenthesis) - len(close_parenthesis), f"Opened {len(open_parenthesis)} parethesis " \
-                f"and closed {len(close_parenthesis)} parenthesis."
+                f"and closed {len(close_parenthesis)} parenthesis"
         minimum_space = sys.maxsize
         closest_op = -1
         closest_cp = -1
@@ -243,15 +241,76 @@ def complexSolveInput(equation):
                     closest_cp = cp
         if closest_cp == -1:
             break
-        small_test = equation_list[closest_op+1:closest_cp]
-        print(small_test)
-        small_solve = simpleSolveInput(small_test)
+        small_equation = equation_list[closest_op + 1:closest_cp]
+        small_solve = simpleSolveInput(small_equation)
+        if small_solve[1] == 'Typo in equation':
+            return small_solve
         equation_list[closest_op] = small_solve[0]
-        print(small_solve)
-        del equation_list[closest_op+1:closest_cp+1]
-        print(equation_list)
+        del equation_list[closest_op + 1:closest_cp + 1]
         continue
     return simpleSolveInput(equation_list)
+
+def colorPrinter(text, color):
+    if color == 'red':
+        return f"\033[91m{text}\033[00m"
+    elif color == 'green':
+        return f"\033[92m{text}\033[00m"
+    else:
+        return 'Color not registered'
+    
+def scoreKeeper(current_pins_up):
+    still_up = 0
+    for pin in current_pins_up:
+        try:
+            int(pin)
+        except ValueError:
+            continue
+        else:
+            still_up += 1
+    score = len(current_pins_up) - still_up
+    return score
+
+def playBall(pin_set, same_frame):
+    die = np.random.randint(1, 7, 3)
+    #die = np.array([3, 2, 1])
+    while True:
+        for pin in pin_set:
+            print(pin, end=' ')
+        print()
+        input_string = f"Enter an equation using {die[0]}, {die[1]}, and {die[2]} or type 'end' to complete the first half of the frame: "
+        equation_input = input(input_string)
+        if equation_input == 'end':
+            break
+        is_valid = verifyInput(equation_input, die)
+        if is_valid != 'Valid':
+            print(colorPrinter(is_valid), 'red')
+            continue
+        solution = complexSolveInput(equation_input)
+        if solution[1] != 'Valid':
+            print(colorPrinter(solution[1] + ', ' + str(solution[0]), 'red'))
+            continue
+        if solution[0] in pin_set:
+            print(colorPrinter(equation_input + ' = ' + str(solution[0]), 'green'))
+            pin_set[pin_set.index(solution[0])] = colorPrinter('\u2713', 'green')
+        else:
+            print(colorPrinter(str(solution[0]) + ' is not a pin to hit', 'red'))
+    ball_score = scoreKeeper(pin_set)
+    if ball_score == len(pin_set):
+        if same_frame:
+            return "SPARE!"
+        return "STRIKE!"
+    return str(ball_score)
+
+def main():
+    args_given = argParser()
+    pins = initializePins(args_given['difficulty'])
+    second_ball_check = input("Is this the second ball using the remaining pins? ")
+    second_ball = False
+    if re.search(r"^[Yy]", second_ball_check):
+        second_ball = True
+        pins = inputPins()
+    report_score = playBall(pins, second_ball)
+    print(colorPrinter(report_score, 'green'))
 
 if __name__ == "__main__":
     main()
